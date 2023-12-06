7c4cdec80d63cefa2b97a2e3ba85edaa1e9fc9ee
Lucas Teodoro <lucasteodoroac@gmail.com>
2023-12-06 12:43:36 -0300

    Commit inicial

---
+++ src/test/java/com/azship/apifrete/ApiFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azship.apifrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
